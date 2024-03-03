5bcbd2ff2239ea923641d8084df0a11b7db430df
itxshakil@gmail.com <itxshakil@gmail.com>
2024-03-03 11:54:27 +0530

    API Add

---
+++ src/api/weather.ts
@@ -0,0 +1,22 @@
+// In api/weather.ts
+
+import { IncomingMessage, ServerResponse } from 'http';
+import axios from 'axios';
+import { NowRequest, NowResponse } from '@vercel/node';
+
+export default async function handler(req: NowRequest, res: NowResponse) {
+  try {
+    const { place } = req.query;
+
+    // Make a GET request to the OpenWeatherMap API with the provided place and API key
+    const { data } = await axios.get(
+      `https://api.openweathermap.org/data/2.5/forecast?q=${place}&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}&cnt=56`
+    );
+
+    // If the request is successful, respond with the fetched data
+    res.status(200).json(data);
+  } catch (error) {
+    // If an error occurs during the request, respond with a 500 Internal Server Error
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -75,13 +75,12 @@ export default function Home() {
   const [place, setPlace] = useAtom(placeAtom);
   const [loadingCity] = useAtom(loadingCityAtom);
 
-  
   const { isLoading, error, data, refetch } = useQuery<WeatherData>(
-    "repoData",
+    "weatherData",
     async () => {
-      const { data } = await axios.get(
-        `https://api.openweathermap.org/data/2.5/forecast?q=${place}&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}&cnt=56`
-      );
+      const { data } = await axios.get('/api/weather', {
+        params: { place },
+      });
       return data;
     }
   );
